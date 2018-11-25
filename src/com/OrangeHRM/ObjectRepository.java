221346721db4903b77c49e58f67c980ecb2c74b0
huntmeabhi <abhishek53312@gmail.com>
2018-11-26 00:12:13 +0530

    My first commit

---
+++ src/com/OrangeHRM/ObjectRepository.java
@@ -0,0 +1,15 @@
+package com.OrangeHRM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class ObjectRepository {
+	
+	
+	public By Username (){
+		return By.xpath("//*[@id='txtUsername']");
+	}
+			
+}
